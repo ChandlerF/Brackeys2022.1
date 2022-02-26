3190ee6ea1e7e4cc32ecd40448684aaac72cdfd8
ChandlerF <63360113+ChandlerF@users.noreply.github.com>
2022-02-26 13:40:39 -0600

    MainMenu

---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+    [SerializeField] private Menu[] _menus;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OpenMenu(string menuName)
+    {
+        for (int i = 0; i < _menus.Length; i++)
+        {
+            if (_menus[i].MenuName.Equals(menuName))
+            {
+                _menus[i].Open();
+            }
+
+            else if (_menus[i].IsOpen)
+            {
+                CloseMenu(_menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < _menus.Length; i++)
+        {
+            if (_menus[i].IsOpen)
+            {
+                CloseMenu(_menus[i]);
+            }
+        }
+        menu.Open();
+    }
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
